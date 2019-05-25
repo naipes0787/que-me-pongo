85283a1de51d46ce0ab6777eef0cb3b4bf2ed7b4
naipes0787 <leandrodavidcosta@gmail.com>
2019-05-25 17:31:23 -0300

    Correcciones en typos, corrección en issue cargada por el ayudante

--- src/test/java/quemepongo/GuardarropaTest.java
+++ src/test/java/quemepongo/GuardarropaTest.java
@@ -96,14 +96,13 @@ public class GuardarropaTest {
 		usuario1.agregarGuardarropa(guardarropa1);
 		usuario1.agregarGuardarropa(guardarropa2);
 		usuario2.agregarGuardarropa(guardarropa3);
-		// En el guardarropa 1 hay sÛlo una prenda de cada categor√≠a
+		// En el guardarropa 1 hay s√≥lo una prenda de cada categor√≠a
 		final Integer cantidadAtuendosGuardarropa1 = 1;
-		// En el guardarropa 2 hay sÛlo una prenda inferior, dos superiores y dos
-		// calzados
+		// En el guardarropa 2 hay s√≥lo una prenda inferior, dos superiores y dos calzados
 		final Integer cantidadAtuendosGuardarropa2 = 1 * 2 * 2;
 		Set<Atuendo> sugerenciasUsuario1 = usuario1.sugerencias();
 		assertEquals(cantidadAtuendosGuardarropa1 + cantidadAtuendosGuardarropa2, sugerenciasUsuario1.size());
-		// En el guardarropa 3 hay sÛlo una prenda inferior, una superior, un calzado y
+		// En el guardarropa 3 hay s√≥lo una prenda inferior, una superior, un calzado y
 		// un accesorio
 		// cantidadAtuendosGuardarropa3 = 1 * 1 * 1 * 1;
 		Set<Atuendo> sugerenciasUsuario2 = usuario2.sugerencias();
@@ -111,7 +110,7 @@ public class GuardarropaTest {
 	}
 
 	
-	@Test // Este guardarropa no tendr· Prendas superiores, por ende se espera que no de ninguna sugerencia
+	@Test // Este guardarropa no tendr√° Prendas superiores, por ende se espera que no de ninguna sugerencia
 	public void sugerenciasDeGuardarropasSinPrendasSuperiores() {
 		Guardarropa guardarropaSinPrendasSuperiores = new Guardarropa();
 
@@ -124,7 +123,7 @@ public class GuardarropaTest {
 		assertEquals(0, sugerencias.size());
 	}
 
-	@Test // Este guardarropa no tendr· Prendas inferiores, por ende se espera que no de ninguna sugerencia
+	@Test // Este guardarropa no tendr√° Prendas inferiores, por ende se espera que no de ninguna sugerencia
 	public void sugerenciasDeGuardarropasSinPrendasInferiores() {
 		Guardarropa guardarropaSinPrendasInferiores = new Guardarropa();
 
@@ -137,7 +136,7 @@ public class GuardarropaTest {
 		assertEquals(0, sugerencias.size());
 	}
 
-	@Test // Este guardarropa no tendr· Prendas de calzado, por ende se espera que no de ninguna sugerencia
+	@Test // Este guardarropa no tendr√° Prendas de calzado, por ende se espera que no de ninguna sugerencia
 	public void sugerenciasDeGuardarropasSinCalzados() {
 		Guardarropa guardarropaSinCalzados = new Guardarropa();
 
--- src/test/java/quemepongo/TipoPrendaTest.java
+++ src/test/java/quemepongo/TipoPrendaTest.java
@@ -18,31 +18,31 @@ public class TipoPrendaTest {
     public void tipoPrendaSuperior(){
         TipoPrenda remera = TipoPrenda.diseniarTipo(new FabricadorTipoSuperior());
         assertEquals(remera.getCategoria(), Categoria.PRENDA_SUPERIOR);
-        assertTipoPrenda(remera, Material.ALGODON, Material.BRONCE);
+        tipoPrendaContieneMaterialesValidos(remera, Material.ALGODON, Material.BRONCE);
     }
 
     @Test
     public void tipoPrendaInferior(){
         TipoPrenda pantalon = TipoPrenda.diseniarTipo(new FabricadorTipoInferior());
         assertEquals(pantalon.getCategoria(), Categoria.PRENDA_INFERIOR);
-        assertTipoPrenda(pantalon, Material.OXFORD, Material.BRONCE);
+        tipoPrendaContieneMaterialesValidos(pantalon, Material.OXFORD, Material.BRONCE);
     }
 
     @Test
     public void tipoCalzado(){
         TipoPrenda zapatos = TipoPrenda.diseniarTipo(new FabricadorTipoCalzado());
         assertEquals(zapatos.getCategoria(), Categoria.CALZADO);
-        assertTipoPrenda(zapatos, Material.CUERO, Material.BRONCE);
+        tipoPrendaContieneMaterialesValidos(zapatos, Material.CUERO, Material.BRONCE);
     }
 
     @Test
     public void tipoAccesorio(){
         TipoPrenda cadenita = TipoPrenda.diseniarTipo(new FabricadorTipoAccesorio());
         assertEquals(cadenita.getCategoria(), Categoria.ACCESORIO);
-        assertTipoPrenda(cadenita, Material.ORO, Material.LANA);
+        tipoPrendaContieneMaterialesValidos(cadenita, Material.ORO, Material.LANA);
     }
 
-    private void assertTipoPrenda(TipoPrenda tipo, Material materialValido, Material materialInvalido) {
+    private void tipoPrendaContieneMaterialesValidos(TipoPrenda tipo, Material materialValido, Material materialInvalido) {
         assertTrue(tipo.getMaterialesValidos().contains(materialValido));
         assertFalse(tipo.getMaterialesValidos().contains(materialInvalido));
     }
