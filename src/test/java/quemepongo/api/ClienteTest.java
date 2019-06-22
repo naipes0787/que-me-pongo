2ddef81cce4da1c0f942d5ba51b837dee1c138e4
Diego Esquivel <desq94@gmail.com>
2019-06-21 21:55:07 -0300

    update test

--- src/test/java/quemepongo/api/ClienteTest.java
+++ src/test/java/quemepongo/api/ClienteTest.java
@@ -2,37 +2,24 @@ package quemepongo.api;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import quemepongo.api.clientes.ApiDeClima;
-import quemepongo.api.clientes.Cliente;
 import quemepongo.model.Localizacion;
 import quemepongo.model.Temperatura;
 
 /**
  * Cliente utilizado para simular una API de Clima y realizar así los tests
  */
-public class ClienteTest extends Cliente implements ApiDeClima{
+public class ClienteTest implements ApiDeClima{
 
-    private static final String host = "";
     private static final Double TEMPERATURA_FIJA = 10D;
 
-    public ClienteTest() {
-        super(host);
-    }
+    public ClienteTest() {}
 
     @Override
     public Temperatura obtenerTemperaturaActual(Localizacion localizacion) {
-    	Temperatura temperatura = new Temperatura(TEMPERATURA_FIJA);
-        return temperatura;
+    	return new Temperatura(TEMPERATURA_FIJA);
     }
 
-    @Override
-    public ObjectMapper buildMapper() {
-        return null;
-    }
-    
     @Test
     public void devuelvaTemperaturaCorrecta(){
     	Temperatura temperatura = this.obtenerTemperaturaActual(Localizacion.CABA);
