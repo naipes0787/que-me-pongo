91acdee3ad3b59b06ad88bda9f3c09cae15b55e4
Andres Bettinelli <bettinelli.andres@gmail.com>
2019-07-08 12:03:08 -0300

    Se agregan test de usuarios y se limpia un poco. Se corrige problema de genero con algunas firmas

--- src/test/java/quemepongo/model/usuario/UsuarioTest.java
+++ src/test/java/quemepongo/model/usuario/UsuarioTest.java
@@ -1,10 +1,13 @@
 package quemepongo.model.usuario;
 
+import com.google.common.collect.Sets;
 import org.junit.Before;
 import org.junit.Test;
+import quemepongo.exceptions.GuardarropaNoPerteneceAlUsuarioException;
 import quemepongo.exceptions.LimiteDeGuardarropasException;
 import quemepongo.model.calificacion.Calificacion;
 import quemepongo.model.calificacion.OpcionesCalificacion;
+import quemepongo.model.guardarropa.GuardarropaCompartido;
 import quemepongo.model.prenda.CreadorDePrenda;
 import quemepongo.model.prenda.FabricadorTipoCalzado;
 import quemepongo.model.prenda.FabricadorTipoSuperiorBase;
@@ -22,6 +25,10 @@ public class UsuarioTest {
 	Usuario johnnyBravo = new Usuario();
 	Usuario montgomeryBurns = new Usuario(new UsuarioPremium());
 
+	//Guardarropa
+	Guardarropa guardarropa = new Guardarropa();
+	GuardarropaCompartido guardarropaCompartidoEntreJohnnyYBurns = new GuardarropaCompartido(Sets.newHashSet(johnnyBravo, montgomeryBurns));
+
 	//TipoPrenda
 	private TipoPrenda REMERA_NEGRA = TipoPrenda.diseniarTipo(new FabricadorTipoSuperiorBase(10));
 	private TipoPrenda PANTUFLAS_CORTE_FINO_DE_RINOCERONTE_ALBINO = TipoPrenda.diseniarTipo(new FabricadorTipoCalzado(10));
@@ -34,81 +41,142 @@ public class UsuarioTest {
 	private Calificacion  congeladoGeneral = new Calificacion(OpcionesCalificacion.CONGELADO, OpcionesCalificacion.CONGELADO,
 			OpcionesCalificacion.CONGELADO, OpcionesCalificacion.CONGELADO);
 
+	//Metodos
+	private void johnnyAgregaUnaPredaNueva(Guardarropa guardarropa){
+		johnnyBravo.agregarPrenda(
+				new CreadorDePrenda()
+						.setTipoPrenda(REMERA_NEGRA)
+						.setMaterial(Material.ALGODON)
+						.setColorPrincipal(Color.BLACK)
+						.build(),
+				guardarropa);
+	}
+
+	private void burnsAgregaUnaPredaNueva(Guardarropa guardarropa){
+		montgomeryBurns.agregarPrenda(
+				new CreadorDePrenda()
+						.setTipoPrenda(PANTUFLAS_CORTE_FINO_DE_RINOCERONTE_ALBINO)
+						.setMaterial(Material.CUERO)
+						.setColorPrincipal(Color.WHITE)
+						.build(),
+				guardarropa);
+	}
+
 	@Before
 	public void ejecutarAntesDeCadaTest() {
 		johnnyBravo = new Usuario();
 		montgomeryBurns = new Usuario(new UsuarioPremium());
+		guardarropa = new Guardarropa();
+		guardarropaCompartidoEntreJohnnyYBurns = new GuardarropaCompartido(Sets.newHashSet(johnnyBravo, montgomeryBurns));
+	}
+
+	@Test
+	public void johnny_TendraUnGuardarropasSiLoAgrega(){
+		assertFalse(johnnyBravo.tieneGuardarropa(guardarropa));
+
+		johnnyBravo.agregarGuardarropa(guardarropa);
+
+		assertTrue(johnnyBravo.tieneGuardarropa(guardarropa));
+	}
+
+	@Test
+	public void johnnyYBurns__YaTendranUnGuardarropasCompartidoAlCrearse(){
+		GuardarropaCompartido guardarropaNuevoCompartido = new GuardarropaCompartido(Sets.newHashSet(johnnyBravo, montgomeryBurns));
+
+		assertTrue(johnnyBravo.tieneGuardarropa(guardarropaNuevoCompartido));
+		assertTrue(montgomeryBurns.tieneGuardarropa(guardarropaNuevoCompartido));
 	}
 
 	@Test(expected = LimiteDeGuardarropasException.class)
-	public void arrojarExcepcionSiElUsuarioGratuitoAgregaMasPrendasAlGuardarropasDeLoPermitido(){
-
-		Guardarropa guardarropa1 = new Guardarropa();
-
-		johnnyBravo.agregarGuardarropa(guardarropa1);
+	public void johnny_quiereAgregarMuchasPrendasASuGuardarropa_PeroEncontraraEncontraraExcepcionPorLimiteAlSerGratuito(){
+		johnnyBravo.agregarGuardarropa(guardarropa);
 
 		for(int i = 0;i < 200;i++) {
-			johnnyBravo.agregarPrenda(
-					new CreadorDePrenda()
-							.setTipoPrenda(REMERA_NEGRA)
-							.setMaterial(Material.ALGODON)
-							.setColorPrincipal(Color.BLACK)
-							.build(),
-					guardarropa1);
+			johnnyAgregaUnaPredaNueva(guardarropa);
+		}
+	}
+
+	@Test(expected = LimiteDeGuardarropasException.class)
+	public void johnny_quiereAgregarMuchasPrendasASuGuardarropaCompartido_PeroEncontraraEncontraraExcepcionPorLimiteAlSerGratuito(){
+
+		for(int i = 0;i < 200;i++) {
+			johnnyAgregaUnaPredaNueva(guardarropaCompartidoEntreJohnnyYBurns);
 		}
 	}
 
 	@Test
-	public void unPremiumSuperaElLimiteDePrendasDelGratuito(){
-
-		Guardarropa guardarropa1 = new Guardarropa();
-
+	public void burns_quiereAgregarMuchasPrendasASuGuardarropa_YNoEncontraraEncontraraExcepcionPorLimiteAlSerPremium(){
+		montgomeryBurns.agregarGuardarropa(guardarropa);
 		for(int i = 0;i < 200;i++) {
-			montgomeryBurns.agregarPrenda(
-					new CreadorDePrenda()
-							.setTipoPrenda(PANTUFLAS_CORTE_FINO_DE_RINOCERONTE_ALBINO)
-							.setMaterial(Material.CUERO)
-							.setColorPrincipal(Color.WHITE)
-							.build(),
-					guardarropa1);
+			burnsAgregaUnaPredaNueva(guardarropa);
 		}
 
-		assertEquals(200, guardarropa1.cantidadDePrendas());
+		assertEquals(200, guardarropa.cantidadDePrendas());
 	}
 
 	@Test
-	public void unPremiumQueAgrega200PrendasAlGuardarropa_Tendra200PrendasEnEseGuardarropaAlNoHaberLimite(){
+	public void burns_quiereAgregarMuchasPrendasASuGuardarropaCompartido_YNoEncontraraEncontraraExcepcionPorLimiteAlSerPremium(){
+		for(int i = 0;i < 200;i++) {
+			burnsAgregaUnaPredaNueva(guardarropaCompartidoEntreJohnnyYBurns);
+		}
 
-		Guardarropa guardarropa1 = new Guardarropa();
+		assertEquals(200, guardarropaCompartidoEntreJohnnyYBurns.cantidadDePrendas());
+	}
 
+	@Test(expected = GuardarropaNoPerteneceAlUsuarioException.class)
+	public void johnneQuiereAgregarUnaPrendaAUnGuardarropa_EncontraExcepcionSiNoTieneEseGuardarropa(){
+		assertFalse(johnnyBravo.tieneGuardarropa(guardarropa));
+
+		for(int i = 0;i < 200;i++) {
+			johnnyAgregaUnaPredaNueva(guardarropa);
+		}
+	}
+
+	@Test(expected = GuardarropaNoPerteneceAlUsuarioException.class)
+	public void burnsQuiereAgregarUnaPrendaAUnGuardarropa_EncontraExcepcionSiNoTieneEseGuardarropa(){
+		assertFalse(montgomeryBurns.tieneGuardarropa(guardarropa));
+
+		for(int i = 0;i < 200;i++) {
+			burnsAgregaUnaPredaNueva(guardarropa);
+		}
+	}
+
+	@Test
+	public void siJohnnyCambiaSuSuscripcionAPremiumYAgrega200PrendasASuGuardarropa_SuGuardarropaTendra200Prendas(){
 		johnnyBravo.cambiarSuscripcion(new UsuarioPremium());
+		johnnyBravo.agregarGuardarropa(guardarropa);
 
 		for(int i = 0;i < 200;i++) {
-			johnnyBravo.agregarPrenda(
-					new CreadorDePrenda()
-							.setTipoPrenda(REMERA_NEGRA)
-							.setMaterial(Material.ALGODON)
-							.setColorPrincipal(Color.BLACK)
-							.build(),
-					guardarropa1);
+			johnnyAgregaUnaPredaNueva(guardarropa);
 		}
 
-		assertEquals(200, guardarropa1.cantidadDePrendas());
+		assertEquals(200, guardarropa.cantidadDePrendas());
 	}
 
 	@Test
-	public void calificacionesAgradablesNoModificanLaSensibilidadDelClima(){
+	public void siJohnnyNoRealizaCalificacionAlguna_NoSeraFrioLentoYSuSensibilidadAlClimaSeraDe1(){
+		assertEquals(1, johnnyBravo.getSensibilidadClima(), 0);
+		assertFalse(johnnyBravo.esFriolentoDeCabeza());
+		assertFalse(johnnyBravo.esFriolentoDeManos());
+		assertFalse(johnnyBravo.esFriolentoDeCuello());
+	}
+
+	@Test
+	public void lasCalificacionesAgragablesDeUnUsuarioNoModificanLaSensibilidadDelClima(){
+		assertEquals(1, johnnyBravo.getSensibilidadClima(), 0);
 
 		johnnyBravo.calificar(agradableGeneral);
-		johnnyBravo.calificar(agradableGeneral);
-		johnnyBravo.calificar(agradableGeneral);
+		assertEquals(1, johnnyBravo.getSensibilidadClima(), 0);
 
+		johnnyBravo.calificar(agradableGeneral);
+		assertEquals(1, johnnyBravo.getSensibilidadClima(), 0);
+
+		johnnyBravo.calificar(agradableGeneral);
 		assertEquals(1, johnnyBravo.getSensibilidadClima(), 0);
 	}
 
 	@Test
-	public void dosCalificacionesCalurosasYUnaFriaDaria0_9(){
-
+	public void jonnhyRealizaDosCalificacionesCalurosasYUnaCongelada_SuSensibilidadClimaDaria0_9(){
 		johnnyBravo.calificar(calurosoGeneral);
 		johnnyBravo.calificar(congeladoGeneral);
 		johnnyBravo.calificar(calurosoGeneral);
@@ -117,8 +185,17 @@ public class UsuarioTest {
 	}
 
 	@Test
-	public void siUnUsuarioCalificaMuchasAtuendosCongeladosEnLasManos_EsFrioLentoEnLasManos(){
+	public void burnsRealizaQuinceCalificacionesCalurosasYUnaCongelada_SuSensibilidadClimaDaria_Negativo0_4(){
+		for(int i = 0; i < 15; i++){
+			johnnyBravo.calificar(calurosoGeneral);
+		}
+		johnnyBravo.calificar(congeladoGeneral);
 
+		assertEquals( -0.4, johnnyBravo.getSensibilidadClima(), 0.01);
+	}
+
+	@Test
+	public void siJohnnyCalificaMuchosAtuendosCongeladosEnLasManos_EsFrioLentoEnLasManos(){
 		johnnyBravo.calificar(congeladoGeneral);
 		johnnyBravo.calificar(congeladoGeneral);
 		johnnyBravo.calificar(congeladoGeneral);
@@ -127,12 +204,11 @@ public class UsuarioTest {
 	}
 
 	@Test
-	public void siUnUsuarioCalificaMuchasAtuendosCalurososEnLasManos_NoEsFrioLentoEnLasManos(){
+	public void siBurnsCalificaMuchosAtuendosCalurososEnLasManos_NoEsFrioLentoEnLasManos(){
+		montgomeryBurns.calificar(calurosoGeneral);
+		montgomeryBurns.calificar(congeladoGeneral);
+		montgomeryBurns.calificar(calurosoGeneral);
 
-		johnnyBravo.calificar(calurosoGeneral);
-		johnnyBravo.calificar(congeladoGeneral);
-		johnnyBravo.calificar(calurosoGeneral);
-
-		assertFalse( johnnyBravo.esFriolentoDeManos() );
+		assertFalse( montgomeryBurns.esFriolentoDeManos() );
 	}
 }
