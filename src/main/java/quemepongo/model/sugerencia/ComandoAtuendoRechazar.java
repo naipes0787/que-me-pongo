82c1d2418ead4b0e47ff716aa67d42f999245da6
Leandro Costa <leandrodavidcosta@gmail.com>
2019-08-16 20:54:00 -0300

    3e - Diagrama de clases + Patrón Command (#27)
    * Agregado de última versión del diagrama de clases + Agregado de patrón command para los estados del Atuendo (Recomendación pendiente del ayudante) + Agregado de archivos de macOS al gitignore

---
+++ src/main/java/quemepongo/model/sugerencia/ComandoAtuendoRechazar.java
@@ -0,0 +1,22 @@
+package quemepongo.model.sugerencia;
+
+public class ComandoAtuendoRechazar extends ComandoAtuendo {
+
+    public ComandoAtuendoRechazar(Atuendo atuendo){
+        this.atuendo = atuendo;
+    }
+
+    @Override
+    public void ejecutar(){
+        if(this.atuendo.getEstado().equals(EstadoAtuendo.NUEVO)) {
+            this.atuendo.setEstado(EstadoAtuendo.RECHAZADO);
+            this.atuendo.setUltimoComando(this);
+        }
+    }
+
+    @Override
+    public void deshacer(){
+        this.atuendo.setEstado(EstadoAtuendo.NUEVO);
+    }
+
+}
