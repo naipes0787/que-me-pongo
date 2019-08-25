20e65ce720d98623250a855cf67990121b086408
leandrocostawolox <leandro.costa@wolox.com.ar>
2019-08-24 21:00:55 -0300

    Se agregan tests para el transformer de fecha + Se eliminan imports innecesarios + Se modifica el transforme de la fecha para que reciba la fecha en lugar de la ocurrencia

--- src/main/java/quemepongo/QueMePongoApplication.java
+++ src/main/java/quemepongo/QueMePongoApplication.java
@@ -1,26 +1,11 @@
 
 package quemepongo;
 
-import java.awt.Color;
-import java.time.Duration;
-import java.time.LocalDateTime;
-
-import com.google.common.collect.Sets;
-
-import quemepongo.model.evento.Evento;
-import quemepongo.model.evento.FechaEspecifica;
-import quemepongo.model.evento.Localizacion;
-import quemepongo.model.prenda.CombinacionPrenda;
-import quemepongo.model.prenda.CreadorDePrenda;
-import quemepongo.model.prenda.FabricadorTipoCalzado;
-import quemepongo.model.prenda.FabricadorTipoInferior;
-import quemepongo.model.prenda.FabricadorTipoSuperiorBase;
-import quemepongo.model.prenda.Material;
-import quemepongo.model.prenda.TipoPrenda;
-import quemepongo.model.sugerencia.Atuendo;
 import quemepongo.ui.arena.evento.ListarEventosWindow;
 
 public class QueMePongoApplication {
+
+
 
 	public static void main(String[] args) {
 		new ListarEventosWindow().startApplication();
