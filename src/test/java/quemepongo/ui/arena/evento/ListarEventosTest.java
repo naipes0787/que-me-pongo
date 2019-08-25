61a59f35e257b77459a9840cfe4c863af76e05d6
leandrocostawolox <leandro.costa@wolox.com.ar>
2019-08-24 21:30:28 -0300

    Se adapta el test para usar el repositorio (Ya que otros tests también insertan eventos)

--- src/test/java/quemepongo/ui/arena/evento/ListarEventosTest.java
+++ src/test/java/quemepongo/ui/arena/evento/ListarEventosTest.java
@@ -1,16 +1,15 @@
 package quemepongo.ui.arena.evento;
 
-import com.google.common.collect.Sets;
 import org.junit.Test;
 import quemepongo.model.evento.Evento;
 import quemepongo.model.evento.FechaEspecifica;
 import quemepongo.model.evento.Localizacion;
-import quemepongo.model.prenda.*;
-import quemepongo.model.sugerencia.Atuendo;
+import quemepongo.model.evento.RepositorioEvento;
 
-import java.awt.*;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,24 +18,35 @@ public class ListarEventosTest {
     @Test
     public void siBuscamosEventosConElMetodoDeLaUI_SeDevuelvenSegunLaFechaIngresada(){
         Evento evento1 = new Evento("Casamiento de Roberto Carlos", Localizacion.CABA,
-                new FechaEspecifica(LocalDateTime.now().plusDays(10)), Duration.ofHours(1));
+                new FechaEspecifica(LocalDateTime.of(2020, Month.JANUARY, 15, 0, 0))
+                , Duration.ofHours(1));
         Evento evento2 = new Evento("Cumpleaños de Martita", Localizacion.CABA,
-                new FechaEspecifica(LocalDateTime.now().plusDays(30)), Duration.ofHours(1));
+                new FechaEspecifica(LocalDateTime.of(2020, Month.JULY, 1, 0, 0))
+                , Duration.ofHours(1));
         Evento evento3 = new Evento("Hackaton UTN", Localizacion.CABA,
-                new FechaEspecifica(LocalDateTime.now().plusDays(90)), Duration.ofHours(1));
+                new FechaEspecifica(LocalDateTime.of(2020, Month.NOVEMBER, 1, 0, 0))
+                , Duration.ofHours(1));
+
+        int cantidadEventos2020 = 0;
+        Set<Evento> eventos = RepositorioEvento.getInstancia().getEventos();
+        for(Evento evento: eventos){
+            if(evento.getFecha().getYear() == 2020){
+                cantidadEventos2020++;
+            }
+        }
 
         ListarEventos listarEventos = new ListarEventos();
-        listarEventos.setDiaDesde("12");
-        listarEventos.setMesDesde("12");
-        listarEventos.setAnioDesde("1999");
-        listarEventos.setDiaHasta("12");
+        listarEventos.setDiaDesde("01");
+        listarEventos.setMesDesde("01");
+        listarEventos.setAnioDesde("2020");
+        listarEventos.setDiaHasta("31");
         listarEventos.setMesHasta("12");
-        listarEventos.setAnioHasta("2019");
+        listarEventos.setAnioHasta("2020");
         listarEventos.search();
-        assertEquals(3, listarEventos.getResultados().size());
-        listarEventos.setMesHasta("11");
+        assertEquals(cantidadEventos2020, listarEventos.getResultados().size());
+        listarEventos.setMesHasta("05");
         listarEventos.search();
-        assertEquals(2, listarEventos.getResultados().size());
+        assertEquals(cantidadEventos2020 - 2, listarEventos.getResultados().size());
     }
 
 
