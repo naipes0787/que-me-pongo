e16aeea72a8ad57501c0d5b51979833543be0828
leandrocostawolox <leandro.costa@wolox.com.ar>
2019-08-31 08:38:56 -0300

    Se agrega mapeo inicial de Prenda y TipoPrenda (quedan pendientes algunos atributos) + Se agregan dependencias al pom para poder trabajar con postgresql o mysql (Queda pendiente definir uno)

--- src/main/java/quemepongo/model/prenda/Prenda.java
+++ src/main/java/quemepongo/model/prenda/Prenda.java
@@ -2,15 +2,32 @@ package quemepongo.model.prenda;
 
 import quemepongo.model.FactorClimatico;
 
+import javax.persistence.*;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
+@Entity
 public class Prenda {
-	
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "tipo_prenda_id", referencedColumnName = "id")
 	private TipoPrenda tipo;
+
+    @Enumerated
+    @Column(columnDefinition = "smallint")
 	private Material material;
+
+    @Transient
     private Color colorPrincipal;
+
+    @Transient
     private Color colorSecundario;
+
+    @Transient
     private BufferedImage foto;
 
     public Prenda(TipoPrenda tipo, Material material, Color colorPrincipal, Color colorSecundario, 
