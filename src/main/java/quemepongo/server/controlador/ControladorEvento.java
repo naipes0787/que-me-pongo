fd4dc6ee79b340784c4aa8dc3f6a743a8c039ceb
LucianaNapolillo <luciananapolillo@gmail.com>
2019-10-25 16:47:50 -0300

    Agrego fecha a formulario de creación del evento

--- src/main/java/quemepongo/server/controlador/ControladorEvento.java
+++ src/main/java/quemepongo/server/controlador/ControladorEvento.java
@@ -3,36 +3,48 @@ package quemepongo.server.controlador;
 import quemepongo.dominio.evento.Evento;
 import quemepongo.dominio.evento.Localizacion;
 import quemepongo.dominio.evento.tipo.Anticipacion;
+import quemepongo.dominio.evento.tipo.EventoUnico;
 import quemepongo.dominio.evento.tipo.TipoEvento;
-import quemepongo.dominio.evento.tipo.TipoEventoFactory;
 import quemepongo.persistencia.RepositorioEvento;
 import quemepongo.server.rutas.RutasConstantes;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 
 public class ControladorEvento implements Controlador {
 
     public ModelAndView getEventoPage(Request request, Response response) {
-        return new ModelAndView(new HashMap<>(), "eventos.hbs");
+        return new ModelAndView(new HashMap<String, Localizacion[]>() {{
+            put("lugares", Localizacion.values());
+        }}, "formulario_evento.hbs");
     }
 
     public Void guardarEvento(Request request, Response response) {
-        String titulo = request.queryParams("titulo");
-        String tipoString = request.queryParams("tipo");
-        String anticipacionString = request.queryParams("anticipacion");
-        String lugarString = request.queryParams("lugar");
-        Anticipacion anticipacion = new Anticipacion(ChronoUnit.DAYS, Integer.valueOf(anticipacionString));
-        TipoEvento tipoEvento = TipoEventoFactory.getTipoEvento(tipoString);
-        Evento evento = new Evento(titulo, Localizacion.valueOf(lugarString), tipoEvento, anticipacion);
+        Evento evento = new Evento(
+                request.queryParams("titulo"),
+                Localizacion.valueOf(request.queryParams("lugar")),
+                new EventoUnico(parsearFecha(request)),
+                new Anticipacion(ChronoUnit.DAYS, Integer.valueOf(request.queryParams("anticipacion"))));
+
         usuarioActivo(request).agregarEvento(evento);
         RepositorioEvento.instancia().guardar(evento);
         response.status(201);
         response.redirect(RutasConstantes.HOME_URL);
         return null;
+    }
+
+    private LocalDateTime parsearFecha(Request request) {
+        String fecha = request.queryParams("fecha");
+        String hora = request.queryParams("hora");
+        String minutos = request.queryParams("minutos");
+        return LocalDate.parse(fecha, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                .atTime(Integer.parseInt(hora), Integer.parseInt(minutos));
     }
 
 }
